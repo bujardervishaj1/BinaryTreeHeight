94266c8e7b9b848bb7ea79442d94b356e12ebd85
Bujar_Dervishaj <bujar.dervishaj@hotmail.com>
2020-12-13 16:49:59 +0100

    binary tree class added

--- BinaryTreeHeight/Program.cs
+++ BinaryTreeHeight/Program.cs
@@ -6,7 +6,16 @@ namespace BinaryTreeHeight
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinaryTree tree = new BinaryTree();
+
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+
+            Console.WriteLine("Height of tree is : " +
+                                        tree.maxDepth(tree.root));
         }
     }
 }
