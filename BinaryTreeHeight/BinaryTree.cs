0da5ff465b7983f7053dcffeb89e4fd5a36052d9
Bujar_Dervishaj <bujar.dervishaj@hotmail.com>
2020-12-13 16:55:47 +0100

    an example function added

--- BinaryTreeHeight/BinaryTree.cs
+++ BinaryTreeHeight/BinaryTree.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BinaryTreeHeight
+﻿namespace BinaryTreeHeight
 {
-    class BinaryTree
+    public class BinaryTree
     {
+        public Node root;
+
+        public int MaxDepth(Node node)
+        {
+            if (node == null)
+                return 0;
+            else
+            {
+                int lDepth = MaxDepth(node.left);
+                int rDepth = MaxDepth(node.right);
+
+                return lDepth > rDepth ? lDepth + 1 : rDepth + 1;
+            }
+        }
     }
 }
--- BinaryTreeHeight/Program.cs
+++ BinaryTreeHeight/Program.cs
@@ -6,16 +6,16 @@ namespace BinaryTreeHeight
     {
         static void Main(string[] args)
         {
-            BinaryTree tree = new BinaryTree();
-
-            tree.root = new Node(1);
-            tree.root.left = new Node(2);
-            tree.root.right = new Node(3);
-            tree.root.left.left = new Node(4);
+            var tree = new BinaryTree
+            {
+                root = new Node(4)
+            };
+            tree.root.left = new Node(3);
+            tree.root.right = new Node(6);
+            tree.root.left.left = new Node(2);
             tree.root.left.right = new Node(5);
 
-            Console.WriteLine("Height of tree is : " +
-                                        tree.maxDepth(tree.root));
+            Console.WriteLine("Lartesia e pemes eshte: " + tree.MaxDepth(tree.root));
         }
     }
 }
