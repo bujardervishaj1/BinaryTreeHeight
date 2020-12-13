5163cda66576a1798c7ac7b99164fafb06b83b65
Bujar_Dervishaj <bujar.dervishaj@hotmail.com>
2020-12-13 16:48:44 +0100

    method for finding the depth of a node added

--- BinaryTreeHeight/Node.cs
+++ BinaryTreeHeight/Node.cs
@@ -2,5 +2,26 @@
 {
     public class Node
     {
+        public int data;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+
+        public static int MaxDepth(Node node)
+        {
+            if (node == null)
+                return 0;
+            else
+            {
+                int lDepth = MaxDepth(node.left);
+                int rDepth = MaxDepth(node.right);
+
+                return lDepth > rDepth ? lDepth + 1 : rDepth + 1;
+            }
+        }
     }
 }
